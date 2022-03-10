18b64ece2bf52db9127d391317d00ea210ea9a0b
Abigail <asyelsub@gmail.com>
2022-03-10 16:22:53 -0500

    added 2d sprite package, renamed a lot of xiao's stuff and added it to files

---
+++ Assets/Resources/AlienABX/Scripts/ABX_xiao23HealingAll.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABX_xiao23HealingAll : Tile
+{
+
+    public override void pickUp(Tile tilePickingUsUp)
+    {
+
+        Debug.Log("Picked up Apple");
+
+        if (!hasTag(TileTags.CanBeHeld))
+        {
+            return;
+        }
+
+        if (tilePickingUsUp is ABX_xiao23InventorySelect || xiao23InventorySelect.MainInventory == null)
+        {
+            transform.parent = Player.instance.transform;
+            transform.localPosition = new Vector3(heldOffset.x, heldOffset.y, -0.1f);
+            transform.localRotation = Quaternion.Euler(0, 0, heldAngle);
+            removeTag(TileTags.CanBeHeld);
+            Player.instance.tileWereHolding = this;
+            _tileHoldingUs = Player.instance;
+            updateSpriteSorting();
+        }
+        else
+        {
+            xiao23InventorySelect.MainInventory.PutItemInBag(this);
+            Destroy(gameObject);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Potion used");
+            tilePickingUsUp.restoreAllHealth();
+            die();
+        }
+
+
+        //base.pickUp(tilePickingUsUp);
+
+        //tilePickingUsUp.restoreAllHealth();
+
+    }
+    public override void useAsItem(Tile tileUsingUs)
+    {
+        base.useAsItem(tileUsingUs);
+        Destroy(gameObject);
+        if (xiao23InventorySelect.MainInventory != null)
+        {
+            xiao23InventorySelect.MainInventory.RemoveItem(this);
+        }
+
+    }
+
+    public override void dropped(Tile tileDroppingUs)
+    {
+        base.dropped(tileDroppingUs);
+        if (xiao23InventorySelect.MainInventory != null)
+        {
+            xiao23InventorySelect.MainInventory.RemoveItem(this);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Resources/AlienABX/Scripts/ABX_xiao23Projectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABX_xiao23Projectile : Tile
+{
+    public float moveSpeed = 1;
+    public int damageCause;
+    [SerializeField]
+    [EnumFlagsAttribute]
+    public TileTags effectTags;
+
+    public void Start()
+    {
+        init();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Tile otherTile = collision.gameObject.GetComponent<Tile>();
+        if (otherTile != null && otherTile.hasTag(effectTags))
+        {
+            otherTile.takeDamage(this, damageCause);
+            takeDamage(otherTile, 1);
+        }
+    }
+    public void Update()
+    {
+        moveViaVelocity(transform.right, moveSpeed, 20);
+    }
+}
