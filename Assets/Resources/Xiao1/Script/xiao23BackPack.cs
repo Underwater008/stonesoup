6eb4ebfb62231327f469a098a88922cc0e74f50a
XiaoMac <1@xiao.studio>
2022-03-10 14:43:14 -0500

    +xiao folder

---
+++ Assets/Resources/Xiao1/Script/xiao23BackPack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class xiao23BackPack : Tile
+{
+    public GameObject InventoryUI;
+    //public GameObject Parent;
+
+
+    public override void pickUp(Tile tilePickingUsUp) {
+
+        Debug.Log("Picked up BackPack");
+        if (tilePickingUsUp is Player)
+        {
+
+            die();
+            Instantiate(InventoryUI);
+        }
+
+        //base.pickUp(tilePickingUsUp);
+
+        //tilePickingUsUp.restoreAllHealth();
+       
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InventoryUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
