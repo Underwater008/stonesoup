bfa5e8b3b275c6d2620a89c17e531336e28ad62a
Xiao <1@xiao.studio>
2022-03-23 23:55:14 -0400

    added pickup sfx and wall tile sprites

--- Assets/Resources/AlienABX/Scripts/ABX_Resource.cs
+++ Assets/Resources/AlienABX/Scripts/ABX_Resource.cs
@@ -7,10 +7,24 @@ public class ABX_Resource : ABX_Tile
     public enum ResourceTags {AlienMineral = 0, AlienPlant, AlienTissue};
     public ResourceTags resourceTag;
 
+    public AudioClip SFPickUpMine;
+
     public bool hasResourceTag (ResourceTags tag)
     {
         if (resourceTag != tag)
             return false;
         return true;
+	}
+
+    private void OnTriggerStay2D(Collider2D _player)
+    {
+        if (_player.gameObject.name == "ABX_Player(Clone)")
+        {
+            Debug.Log("MineTouchingPlayer");
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                AudioManager.playAudio(SFPickUpMine);
+            }
+        }
     }
 }
